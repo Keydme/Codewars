59f08c675a397f854879bfe12c11bf3ef2a7526d
Keydme <87051268+Keydme@users.noreply.github.com>
2023-02-18 13:52:58 +0800

    修正line_space函数

--- 波的叠加ksh.py
+++ 波的叠加ksh.py
@@ -5,21 +5,22 @@ from matplotlib.animation import FuncAnimation
 
 # 定义画布
 fig, ax = plt.subplots()
+plt.grid(ls='--')
 line, =ax.plot([], [])
 #line1, = ax.plot([], [])  # 返回的第一个值是update函数需要改变的
 #line2, = ax.plot([], [])
 
 
 # 获取直线的数组
-def line_space(x1, x2, B, C, A):
+def line_space(x1, x2, B, A=1, OMEGA=1, PHI=0): #x1,x2范围 , B步长 , Phi φ补偿位置 相位 , omiga  ω角频率角速度, A振幅
     x = np.linspace(x1, x2, B)
-    return x, np.sin(A*(x+C))
+    return x, A*np.sin(OMEGA*(x+PHI))
 
 def addy(B):
     x = np.linspace(0, 20, 1000)
     y = np.linspace(0,0,1000)
-    x1, y2 = line_space(20-20*B/1000, 20, B+1, 20*B/1000-20, 1)#+np.pi)
-    x2, y1 = line_space(0, 20*B/1000, B+1, -20*B/1000, 2)
+    x1, y2 = line_space(20-20*B/1000, 20, B+1, 20*B/1000-20)#+np.pi)
+    x2, y1 = line_space(0, 20*B/1000, B+1, 2, -20*B/1000, 2)
     if B>500: 
         for i in range(1000-B,B):
             y[i]=y1[i]+y2[i-1000+B]
@@ -36,7 +37,7 @@ def addy(B):
 # 这里B就是frame
 def update(B):
     ax.set_xlim(0, 20)
-    ax.set_ylim(-2, 2)
+    ax.set_ylim(-4, 4)
     #x1, y1 = line_space(20-20*B/1000,20,1000-B)
     #x2, y2 = line_space(0,20*B/1000,B)
     x, y = addy(B)
@@ -51,4 +52,4 @@ def update(B):
 #addy(15)
 
 ani = FuncAnimation(fig, update, frames=range(1000), interval=10)
-ani.save('move1.mp4', fps=100)
+ani.save('move2.mp4', fps=100)
